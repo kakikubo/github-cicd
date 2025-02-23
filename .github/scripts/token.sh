9a71297ffed1b77525c282a7de5c12db74f2861b
kakikubo <kakikubo@gmail.com>
2025-02-23 09:08:07 +0900

    signatureとjwtのログをマスク

--- .github/scripts/token.sh
+++ .github/scripts/token.sh
@@ -16,7 +16,9 @@ exp="$((now + (3 * 60)))"
 template='{"iss":"%s","iat":%s,"exp":%s}'
 payload="$(printf "${template}" "${APP_ID}" "${iat}" "${exp}" | base64url)"
 signature="$(printf '%s' "${header}.${payload}" | sign | base64url)"
+echo "::add-mask::${signature}"
 jwt="${header}.${payload}.${signature}"
+echo "::add-mask::${jwt}"
 
 # Installation APIの実行
 repo="${GITHUB_REPOSITORY_OWNER}/${TARGET_REPO}"
